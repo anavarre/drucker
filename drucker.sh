b5dce901eb2e551bea89e25d214736b5256bac7d
Aurelien Navarre <aurelien.navarre@acquia.com>
2016-04-11 18:48:29 +0200

    Abstract out more code and start using custom images as the foundation for containers deployment

--- drucker.sh
+++ drucker.sh
@@ -3,11 +3,16 @@
 # Invoke the script from anywhere (e.g .bashrc alias)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source "${DIR}"/functions.sh
+source "${DIR}"/variables
+source "${DIR}"/init
+source "${DIR}"/ssh
+source "${DIR}"/orchestration
+source "${DIR}"/containers
 
 check_requirements
 create_custom_bridge_network
 pull_base_image_from_docker_hub
-build_drucker_base_image
+build_drucker_init_image
+provision_base_container
 provision_reverse_proxy_container
 provision_web_container
