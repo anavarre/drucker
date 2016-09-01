257287811cbc9419807bbbdb7d1c7b6780ce1d79
Aurelien Navarre <aurelien.navarre@acquia.com>
2016-09-01 18:53:35 +0200

    Allow Drush to bootstrap Drupal via the remote MySQL database

--- drucker.sh
+++ drucker.sh
@@ -38,4 +38,5 @@ provision_base_container
 provision_reverse_proxy_container
 provision_db_container
 provision_web_container
+allow_web_to_db_ssh_access
 # provision_web2_container has been excluded for now
