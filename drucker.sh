2e2d85f0899cf9924ff8babc6269dc4b694224d5
Aurelien Navarre <aurelien.navarre@acquia.com>
2016-12-14 09:55:47 +0100

    Make user configuration more streamlined

--- drucker.sh
+++ drucker.sh
@@ -4,24 +4,34 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # shellcheck source=/dev/null
-source "${DIR}"/functions
+source "${DIR}/config"
+# shellcheck source=/dev/null
+source "${DIR}/functions"
 
 export OPTION=$1
 export SITENAME=$2
 
-container_files
+# Custom user configuration.
+set_local_ssh_path
+set_local_html_path
+set_local_db_path
 
+load_container_files
+
+# Are we running drucker with a CLI argument?
 drucker_argument
 
 check_requirements
-configure_ssh_access
+
+# drucker initialization.
 create_custom_bridge_network
 pull_base_image_from_docker_hub
 build_init_image
+# Container provisioning and orchestration.
 provision_base_container
 provision_reverse_proxy_container
 provision_db_container
 provision_web_container
-allow_web_to_db_ssh_access
 provision_search_container
+allow_web_to_db_ssh_access
 # provision_web2_container has been excluded for now
