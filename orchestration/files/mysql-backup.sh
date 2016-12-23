9985e2da7e9ee44df05f234d6c8570d1fcf88cc1
Aurelien Navarre <aurelien.navarre@acquia.com>
2016-12-23 15:42:33 +0100

    Add automated MySQL backup script

---
+++ orchestration/files/mysql-backup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CREDS="root"
+HOST="localhost"
+MYSQL="$(which mysql)"
+MYSQLDUMP="$(which mysqldump)"
+BACKUP_DIR="/var/lib/mysql/backup"
+QUERY="SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME NOT REGEXP 'backup|mysql|information_schema|performance_schema'"
+GZIP="$(which gzip)"
+NOW=$(date +"%Y-%m-%d_%R")
+
+DBS="$(${MYSQL} -h ${HOST} -u ${CREDS} -p${CREDS} -Bse "${QUERY}")"
+for DB in ${DBS}
+do
+  FILE=${BACKUP_DIR}/${DB}_${NOW}.gz
+  ${MYSQLDUMP} -h ${HOST} -u ${CREDS} -p${CREDS} ${DB} | ${GZIP} -9 > ${FILE}
+done
