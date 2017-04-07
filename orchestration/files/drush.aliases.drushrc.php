e70424f01ac74768dd6fe43841bd12a1b935f3f6
Aurelien Navarre <aurelien.navarre@acquia.com>
2017-04-07 10:34:45 +0200

    Refactor drupal-create and drupal-composer to with a different docroot architecture

--- orchestration/files/drush.aliases.drushrc.php
+++ orchestration/files/drush.aliases.drushrc.php
@@ -3,6 +3,6 @@
 $aliases['local'] = array(
   'site' => 'SITENAME',
   'env' => 'local',
-  'root' => '/var/www/html/SITENAME',
+  'root' => '/var/www/html/SITENAME/docroot',
   'uri' => 'SITENAME.local',
 );
