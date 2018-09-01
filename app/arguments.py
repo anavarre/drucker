ec4ac43089f605b40a1c0a597ab462d6e11ed34b
Aurelien Navarre <aurelien.navarre@acquia.com>
2018-09-01 09:22:56 +0200

    Random refactoring and work towards supporting argparse correctly

--- app/arguments.py
+++ app/arguments.py
@@ -2,79 +2,70 @@
 """Allows to pass arguments to the drucker command"""
 
 import subprocess as s
-import colorful as c
-import argparse
-import variables as v
 import sys
+import argparse
+import colorful as c
+import variables as v
 import orchestration as o
 import containers
 
-p = argparse.ArgumentParser()
-
-p.add_argument('--health',
-    dest='health',
-    action="store_true",
-    help='Runs a service healthcheck')
-
-p.add_argument('--start',
-    dest='start',
-    action="store_true",
-    help='Starts all containers')
-
-p.add_argument('--stop',
-    dest='stop',
-    action="store_true",
-    help='Stops all containers')
-
-p.add_argument('--restart',
-    dest='restart',
-    action="store_true",
-    help='Restarts all containers')
-
-p.add_argument('--list',
-    dest='list',
-    action="store_true",
-    help='Lists all deployed apps')
-
-p.add_argument('--delete',
-    dest='delete',
-    action="store_true",
-    help='Deletes an arbitrary docroot')
-
-p.add_argument('--tests',
-    dest='tests',
-    action="store_true",
-    help='Runs the Ansible test suite')
-
-p.add_argument('--version',
-    dest='version',
-    action="store_true",
-    help='Returns the drucker version')
-
-args = p.parse_args()
-
 def return_version():
     """Returns either the latest commit hash or tagged release"""
-    latest_commit= s.getoutput("cd %s && git rev-parse --short HEAD" % (v.APP_ROOT))
+    latest_commit = s.getoutput("cd %s && git rev-parse --short HEAD" % (v.APP_ROOT))
 
     if 'dev' in v.APP_VERSION:
         print("You are running the dev version at commit " + c.orange(latest_commit))
     else:
         print("You are running tagged release " + c.orange(v.APP_VERSION))
 
-if args.health:
-    containers.health()
-elif args.start:
-    containers.start()
-elif args.stop:
-    containers.stop()
-elif args.restart:
-    containers.restart()
-elif args.list:
-    o.app_list()
-elif args.delete:
-    o.app_delete()
-elif args.tests:
-    o.run_tests()
-elif args.version:
-    return_version()
+def parser():
+    """Parses command-line arguments."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("app", nargs='?',
+                        help="Targets an arbitrary app")
+
+    parser.add_argument("--health", dest="health", action="store_true",
+                        help="Runs a service healthcheck")
+
+    parser.add_argument('--start', dest='start', action="store_true",
+                        help='Starts all containers')
+
+    parser.add_argument('--stop', dest='stop', action="store_true",
+                        help='Stops all containers')
+
+    parser.add_argument('--restart', dest='restart', action="store_true",
+                        help='Restarts all containers')
+
+    parser.add_argument('--delete', dest='delete', action="store_true",
+                        help='Deletes an arbitrary docroot')
+
+    parser.add_argument('--version', dest='version', action="store_true",
+                        help='Returns the drucker version')
+
+    parser.add_argument('--list', dest='list', action="store_true",
+                        help='Lists all deployed apps')
+
+    parser.add_argument('--tests', dest='tests', action="store_true",
+                        help='Runs the Ansible test suite')
+
+    args = parser.parse_args()
+
+    if args.health:
+        containers.health()
+    elif args.start:
+        containers.start()
+    elif args.stop:
+        containers.stop()
+    elif args.restart:
+        containers.restart()
+    elif args.list:
+        o.app_list()
+    elif args.delete:
+        o.app_delete(args.app)
+    elif args.tests:
+        o.run_tests()
+    elif args.version:
+        return_version()
+
+parser()
--- app/orchestration.py
+++ app/orchestration.py
@@ -2,15 +2,16 @@
 """Manages orchestration for all containers"""
 
 import subprocess as s
-import colorful as c
-import variables as v
-import orchestration as o
+import sys
 import os
-import containers
+import colorful as c
 import click
+import variables as v
+
+import containers
 
 def run_orchestration(container, shortname):
-    """Parent function to manage container orchestration"""
+    """Parent function to manage container orchestration."""
     print(c.blue("Running %s orchestration on the container..." % (container)))
     s.getoutput("export ANSIBLE_HOST_KEY_CHECKING=False")
     s.run('''
@@ -19,36 +20,39 @@ def run_orchestration(container, shortname):
 
 
 def run_base_orchestration():
-    """Run orchestration on base container"""
+    """Runs orchestration on base container"""
     run_orchestration(v.BASE_CONTAINER, "base")
 
 
 def run_mirror_orchestration():
-    """Run orchestration on mirror container"""
+    """Runs orchestration on mirror container"""
     run_orchestration(v.MIRROR_CONTAINER, "mirror")
 
 
 def run_edge_orchestration():
-    """Run orchestration on edge container"""
+    """Runs orchestration on edge container"""
     run_orchestration(v.EDGE_CONTAINER, "edge")
 
+
 def run_db_orchestration():
-    """Run orchestration on database container"""
+    """Runs orchestration on database container"""
     run_orchestration(v.DB_CONTAINER, "db")
 
+
 def run_search_orchestration():
-    """Run orchestration on search container"""
+    """Runs orchestration on search container"""
     run_orchestration(v.SEARCH_CONTAINER, "search")
 
 
 def run_ssh_orchestration():
-    """Run SSH orchestration"""
+    """Runs SSH orchestration"""
     run_orchestration("SSH", "ssh")
 
 
 def run_web_orchestration():
-    """Run orchestration on web container"""
+    """Runs orchestration on web container"""
     run_orchestration(v.WEB_CONTAINER, "web")
+
 
 def run_tests_orchestration(shortname):
     """Parent function to manage running the test suite"""
@@ -63,7 +67,8 @@ def run_tests():
     """Runs the Ansible test suite"""
     containers.status()
 
-    php_version_check = s.getoutput('''docker exec -u %s -it %s /bin/cat /etc/php/default_php_version
+    php_version_check = s.getoutput('''docker exec -u %s -it %s /bin/cat\
+                                       /etc/php/default_php_version
                                     ''' % (v.APP, v.WEB_CONTAINER))
     if v.DEFAULT_PHP not in php_version_check:
         print("Tests need to be executed against the current stable PHP version")
@@ -71,9 +76,10 @@ def run_tests():
         sys.exit()
 
     for group in v.TEST_GROUPS:
-        o.run_tests_orchestration(group)
+        run_tests_orchestration(group)
 
 def app_list():
+    """Returns a list of installed apps."""
     s.run('''docker exec -it %s cat %s/.app-registry
           ''' % (v.WEB_CONTAINER, v.CONTAINER_HTML_PATH), shell=True)
 
@@ -101,18 +107,21 @@ def hosts_file():
     print(c.green("Remember to add the %s.local entry to your local /etc/hosts file!"))
 
 
-def app_delete():
+def app_delete(app):
     """Deletes an arbitrary docroot"""
-    """TODO: actually enforce arbitrary docroots through positional arguments"""
-    if os.path.isdir("%s/%s" % (v.CONTAINER_HTML_PATH, v.APP)):
-        if click.confirm("The %s docroot already exists. Should we overwrite the codebase, files and database?" % (v.APP), default=True):
-            print(c.blue("Deleting %s docroot..." % (v.APP)))
+
+    # TODO: actually enforce arbitrary docroots through positional arguments
+
+    if os.path.isdir("%s/%s" % (v.CONTAINER_HTML_PATH, app)):
+        if click.confirm("The %s docroot already exists. Should we overwrite the codebase, files and database?" % (app), default=True):
+            print(c.blue("Deleting %s docroot..." % (app)))
             s.run('''ansible-playbook -i %s/orchestration/hosts\
                      --user=%s %s/orchestration/commands/app-delete.yml\
                      --extra-vars "ansible_sudo_pass=%s app=delete sitename=%s"
-                  ''' % (v.APP_DIR, v.APP, v.APP_DIR, v.APP, v.APP), shell=True)
-            print(c.green("Remember to remove the %s.local entry from your local /etc/hosts file!" % (v.APP)))
+                  ''' % (v.APP_DIR, v.APP, v.APP_DIR, v.APP, app), shell=True)
+            print(c.green("Remember to remove the %s.local entry from your\
+                           local /etc/hosts file!" % (app)))
         else:
-           print(c.green("Back to the comfort zone. Aborting..."))
+            print(c.green("Back to the comfort zone. Aborting..."))
     else:
         print("This app doesn't exist.")
