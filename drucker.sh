7d42904f104be8e81a882d0c500cc6e4794f6940
Aurelien Navarre <aurelien.navarre@acquia.com>
2018-09-03 14:03:08 +0200

    Make parser work

--- drucker.sh
+++ drucker.sh
@@ -5,34 +5,39 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 APP="`command -v python3` ${DIR}/app"
 
-source "${DIR}/init"
-load_function_files
-load_container_files
-
-${APP}/arguments.py
+# Make sure the app can run successfully.
 ${APP}/requirements.py
-
-source "${DIR}/config"
-
-export COMMAND=$1
-export SITENAME=$2
-
-# Force lowercase on sitename(s).
-export SITENAME=$(echo "$SITENAME" | tr '[:upper:]' '[:lower:]')
-
-if [[ $3 = *.* ]]; then
-  export GIT_TAG=$3
-fi
-
-# Custom user configuration.
 ${APP}/local_setup.py
 
+# Are we running drucker with a CLI argument?
+if [[ $# -eq 1 ]]; then
+  export COMMAND=$1
+  ${APP}/arguments.py ${COMMAND}
+  exit 1
+elif [[ $# -eq 2 ]]; then
+  export COMMAND=$1
+  export SITENAME=$2
+  ${APP}/arguments.py ${COMMAND} ${SITENAME}
+  exit 1
+fi
+
+# source "${DIR}/init"
+# load_function_files
+# load_container_files
+
+# source "${DIR}/config"
+
+# Force lowercase on sitename(s).
+# export SITENAME=$(echo "$SITENAME" | tr '[:upper:]' '[:lower:]')
+
+# if [[ $3 = *.* ]]; then
+#   export GIT_TAG=$3
+# fi
 
 #$(command -v python3) ${DIR}/app/ssh.py
 
-
 # Are we running drucker with a CLI argument?
-drucker_argument
+# drucker_argument
 
 # # drucker initialization.
 ${APP}/init.py
