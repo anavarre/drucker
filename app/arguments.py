31a1ec4371b662b01a4ce5f62e467589e86c490d
Niels van Mourik <niels.vanmourik@acquia.com>
2018-09-06 12:47:20 +0200

    For readability, I'm renaming 'mainfunc' into 'dispatched_function' to make crystal-clear that this is the main function running during Drucker runtime.

--- app/arguments.py
+++ app/arguments.py
@@ -28,55 +28,55 @@ def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("app", nargs='?',
                         help="Targets an arbitrary app")
-    parser.add_argument('--drupal', dest='mainfunc', action='store_const',
+    parser.add_argument('--drupal', dest='dispatched_function', action='store_const',
                         help='Spins up a ready-to-use Drupal install',
                         const=orch.app_drupal)
-    parser.add_argument('--lightning', dest='mainfunc', action='store_const',
+    parser.add_argument('--lightning', dest='dispatched_function', action='store_const',
                         help='Spins up a ready-to-use Lightning install',
                         const=orch.app_lightning)
-    parser.add_argument('--blt', dest='mainfunc', action='store_const',
+    parser.add_argument('--blt', dest='dispatched_function', action='store_const',
                         help='Spins up a ready-to-use BLT build',
                         const=orch.app_blt)
-    parser.add_argument('--dev', dest='mainfunc', action='store_const',
+    parser.add_argument('--dev', dest='dispatched_function', action='store_const',
                         help='Prepare app for development work with no'
                              ' caching and helper modules enabled.',
                         const=orch.app_dev)
-    parser.add_argument('--prod', dest='mainfunc', action='store_const',
+    parser.add_argument('--prod', dest='dispatched_function', action='store_const',
                         help='Opinionated setup with all known performance'
                              ' best practices enabled.',
                         const=orch.app_prod)
-    parser.add_argument('--import', dest='mainfunc', action='store_const',
+    parser.add_argument('--import', dest='dispatched_function', action='store_const',
                         help="Imports an app from the web container's"
                              " import directory",
                         const=orch.app_import)
-    parser.add_argument('--delete', dest='mainfunc', action='store_const',
+    parser.add_argument('--delete', dest='dispatched_function', action='store_const',
                         help='Deletes an arbitrary docroot',
                         const=orch.app_delete)
-    parser.add_argument('--start', dest='mainfunc', action='store_const',
+    parser.add_argument('--start', dest='dispatched_function', action='store_const',
                         help='Starts all containers',
                         const=cont.start)
-    parser.add_argument('--stop', dest='mainfunc', action='store_const',
+    parser.add_argument('--stop', dest='dispatched_function', action='store_const',
                         help='Stops all containers',
                         const=cont.stop)
-    parser.add_argument('--restart', dest='mainfunc', action='store_const',
+    parser.add_argument('--restart', dest='dispatched_function', action='store_const',
                         help='Restarts all containers',
                         const=cont.restart)
-    parser.add_argument("--health", dest='mainfunc', action='store_const',
+    parser.add_argument("--health", dest='dispatched_function', action='store_const',
                         help="Runs a service healthcheck",
                         const=cont.health)
-    parser.add_argument('--php7.2', dest='mainfunc', action='store_const',
+    parser.add_argument('--php7.2', dest='dispatched_function', action='store_const',
                         help='Sets the PHP version to 7.2',
                         const=cont.set_default_php_version)
-    parser.add_argument('--php7.1', dest='mainfunc', action='store_const',
+    parser.add_argument('--php7.1', dest='dispatched_function', action='store_const',
                         help='Sets the PHP version to 7.1',
                         const=cont.set_previous_php_version)
-    parser.add_argument('--list', dest='mainfunc', action='store_const',
+    parser.add_argument('--list', dest='dispatched_function', action='store_const',
                         help='Lists all deployed apps',
                         const=orch.app_list)
-    parser.add_argument('--tests', dest='mainfunc', action='store_const',
+    parser.add_argument('--tests', dest='dispatched_function', action='store_const',
                         help='Runs the Ansible test suite',
                         const=orch.run_tests)
-    parser.add_argument('--version', dest='mainfunc', action='store_const',
+    parser.add_argument('--version', dest='dispatched_function', action='store_const',
                         help='Returns the drucker version',
                         const=return_version)
     return parser
