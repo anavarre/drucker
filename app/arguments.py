afe907057197f9d6110b0fbc6559040df38aee53
Aurelien Navarre <aurelien.navarre@acquia.com>
2018-09-06 09:54:46 +0200

    Make variables more consistent

--- app/arguments.py
+++ app/arguments.py
@@ -5,21 +5,21 @@ import sys
 import argparse
 import subprocess
 import colorful
-import variables
-import containers
-import orchestration
+import variables as vars
+import containers as cont
+import orchestration as orch
 
 
 def return_version(args):
     """Returns either the latest commit hash or tagged release"""
     latest_commit = subprocess.getoutput("cd %s && git rev-parse --short HEAD"
-                                         % (variables.APP_ROOT))
-    if 'dev' in variables.APP_VERSION:
-        ver = colorful.orange(latest_commit)
-        print("You are running the dev version at commit " + ver)
+                                         % (vars.APP_ROOT))
+    if 'dev' in vars.APP_VERSION:
+        hash = colorful.orange(latest_commit)
+        print("You are running the dev version at commit " + hash)
     else:
-        ver = colorful.orange(variables.APP_VERSION)
-        print("You are running tagged release " + ver)
+        tag = colorful.orange(vars.APP_VERSION)
+        print("You are running tagged release " + tag)
     return args.exit_ok
 
 
@@ -30,52 +30,52 @@ def get_parser():
                         help="Targets an arbitrary app")
     parser.add_argument('--drupal', dest='mainfunc', action='store_const',
                         help='Spins up a ready-to-use Drupal install',
-                        const=orchestration.app_drupal)
+                        const=orch.app_drupal)
     parser.add_argument('--lightning', dest='mainfunc', action='store_const',
                         help='Spins up a ready-to-use Lightning install',
-                        const=orchestration.app_lightning)
+                        const=orch.app_lightning)
     parser.add_argument('--blt', dest='mainfunc', action='store_const',
                         help='Spins up a ready-to-use BLT build',
-                        const=orchestration.app_blt)
+                        const=orch.app_blt)
     parser.add_argument('--dev', dest='mainfunc', action='store_const',
                         help='Prepare app for development work with no'
                              ' caching and helper modules enabled.',
-                        const=orchestration.app_dev)
+                        const=orch.app_dev)
     parser.add_argument('--prod', dest='mainfunc', action='store_const',
                         help='Opinionated setup with all known performance'
                              ' best practices enabled.',
-                        const=orchestration.app_prod)
+                        const=orch.app_prod)
     parser.add_argument('--import', dest='mainfunc', action='store_const',
                         help="Imports an app from the web container's"
                              " import directory",
-                        const=orchestration.app_import)
+                        const=orch.app_import)
     parser.add_argument('--delete', dest='mainfunc', action='store_const',
                         help='Deletes an arbitrary docroot',
-                        const=orchestration.app_delete)
+                        const=orch.app_delete)
     parser.add_argument('--start', dest='mainfunc', action='store_const',
                         help='Starts all containers',
-                        const=containers.start)
+                        const=cont.start)
     parser.add_argument('--stop', dest='mainfunc', action='store_const',
                         help='Stops all containers',
-                        const=containers.stop)
+                        const=cont.stop)
     parser.add_argument('--restart', dest='mainfunc', action='store_const',
                         help='Restarts all containers',
-                        const=containers.restart)
+                        const=cont.restart)
     parser.add_argument("--health", dest='mainfunc', action='store_const',
                         help="Runs a service healthcheck",
-                        const=containers.health)
+                        const=cont.health)
     parser.add_argument('--php7.2', dest='mainfunc', action='store_const',
                         help='Sets the PHP version to 7.2',
-                        const=containers.set_default_php_version)
+                        const=cont.set_default_php_version)
     parser.add_argument('--php7.1', dest='mainfunc', action='store_const',
                         help='Sets the PHP version to 7.1',
-                        const=containers.set_previous_php_version)
+                        const=cont.set_previous_php_version)
     parser.add_argument('--list', dest='mainfunc', action='store_const',
                         help='Lists all deployed apps',
-                        const=orchestration.app_list)
+                        const=orch.app_list)
     parser.add_argument('--tests', dest='mainfunc', action='store_const',
                         help='Runs the Ansible test suite',
-                        const=orchestration.run_tests)
+                        const=orch.run_tests)
     parser.add_argument('--version', dest='mainfunc', action='store_const',
                         help='Returns the drucker version',
                         const=return_version)
