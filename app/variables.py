67966d6f969bfbd2e5aa93b7cd97446f1a1c8a44
Aurelien Navarre <aurelien.navarre@acquia.com>
2018-09-13 16:02:47 +0200

    Prep for 0.6.9

--- app/variables.py
+++ app/variables.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 
 APP = "drucker"
-APP_VERSION = "dev"
+APP_VERSION = "0.6.9"
 APP_ROOT = os.path.dirname(os.path.dirname(__file__))
 APP_DIR = "%s/app" % (APP_ROOT)
 HOME = str(Path.home())
